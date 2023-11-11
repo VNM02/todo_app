36b1ab729f22e5f3e8025c6f2232591bf4f5e2b5
Vaibhav <nvaibhavmahajan00@gmail.com>
2023-11-11 23:03:56 +0530

    Todo_App_Commit

---
+++ lib/bloc/authentication/authentication_event.dart
@@ -0,0 +1,5 @@
+abstract class AuthenticationEvent{}
+
+class AuthenticationSignUpEvent extends AuthenticationEvent{}
+
+class AuthenticationLoginEvent extends AuthenticationEvent{}
---
+++ lib/bloc/authentication/authentication_state.dart
@@ -0,0 +1,8 @@
+
+abstract class AuthenticationState{}
+
+class AuthenticationInitialState extends AuthenticationState{}
+
+class AuthenticationSignUpState extends AuthenticationState{}
+
+class AuthenticationLoginState extends AuthenticationState{}
